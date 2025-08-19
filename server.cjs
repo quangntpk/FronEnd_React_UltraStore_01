8640b6c1bd5a18e96953e0c6782239269ed1431a
EnaNguyen <nguyenquangquyx@gmail.com>
2025-08-19 12:41:00 +0700

    hotfix2

--- server.cjs
+++ server.cjs
@@ -1,11 +1,6 @@
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
-import { fileURLToPath } from 'url';
-
-// Get __dirname equivalent in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
